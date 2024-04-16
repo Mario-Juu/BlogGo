5630f62066d56a7cff2b43a2030c51c933a7a108
Mario Júnior <mariojralves2006@gmail.com>
2024-04-16 12:09:50 -0300

    Novas rotas para delete, edit e view

--- routes.go
+++ routes.go
@@ -11,8 +11,11 @@ func (a *Application) Routes() http.Handler {
 	mux.HandleFunc("/contact", a.ContactHandler(ContactView))
 	mux.HandleFunc("/post", a.AuthMiddleware(a.PostHandler(PostView)))
 	mux.HandleFunc("/signup", a.SignUpHandler(SignUpView))
+	mux.HandleFunc("/post/viewer", a.PostViewerHandler(PostViewerView))
 	mux.HandleFunc("/logout", a.LogoutHandler)
 	mux.HandleFunc("/post/new", a.AuthMiddleware(a.NewPostHandler(NewPostView)))
+	mux.HandleFunc("/post/edit", a.AuthMiddleware(a.EditPostHandler(EditPostView)))
+	mux.HandleFunc("/post/delete", a.AuthMiddleware(DeletePostHandler))
 	mux.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("static"))))
