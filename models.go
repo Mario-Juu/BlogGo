8bbe400f9c853e84e9eb9d50b4e0eec0d608cf37
Mario Júnior <mariojralves2006@gmail.com>
2024-04-16 12:10:07 -0300

    Alteração do crud com uso de join e afins

--- models.go
+++ models.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"html/template"
 	"log"
 	"time"
 )
@@ -14,11 +16,12 @@ type User struct {
 type Post struct {
 	Id        int
 	Title     string
-	Content   string
+	Content   template.HTML
 	Slug      string
 	Author    *User
 	CreatedAt time.Time
 	UpdatedAt time.Time
+
 }
 
 func FindUserByEmail(email string) (*User, error) {
@@ -44,20 +47,85 @@ func CreatePost(post Post) error {
 }
 
 func ReadPosts() []Post {
-	posts := make([]Post, 0)
-	rows, err := db.Query("SELECT * FROM posts")
+	posts := []Post{}
+	rows, err := db.Query(`select p.id, p.title, p.slug, p.content, p.user_id, u.email, p.created_at, p.updated_at 
+							from posts p join users u on p.user_id = u.id`)
 	if err != nil {
-		log.Print(err)
+		log.Println(err)
+		return posts
 	}
+
 	for rows.Next() {
-		var user User
 		var post Post
-		err = rows.Scan(&post.Id, &post.Title, &post.Slug, &post.Content, &user.Id, &post.CreatedAt, &post.UpdatedAt)
+		var user User
+		err := rows.Scan(
+			&post.Id,
+			&post.Title,
+			&post.Slug,
+			&post.Content,
+			&user.Id,
+			&user.Email,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+		)
 		if err != nil {
 			log.Println(err)
+			return posts
 		}
 		post.Author = &user
 		posts = append(posts, post)
 	}
+
 	return posts
+}
+
+func GetPostById(id int) (*Post, error) {
+	row := db.QueryRow("SELECT p.id, p.title, p.slug, p.content, p.user_id, u.email, p.created_at, p.updated_at FROM posts p JOIN users u ON p.user_id = u.id WHERE p.id = ?", id)
+
+	var post Post
+	var user User
+	err := row.Scan(
+			&post.Id,
+			&post.Title,
+			&post.Slug,
+			&post.Content,
+			&user.Id,
+			&user.Email,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+		)
+		if err != nil {
+			log.Println(err)
+			return &post, err
+		}
+		post.Author = &user
+	return &post, nil
+}
+
+func UpdatePost(post Post) error {
+	stmt, err := db.Prepare("UPDATE posts SET title = ?, content = ?, slug = ?, updated_at = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(post.Title, post.Content, post.Slug, post.UpdatedAt, post.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeletePost(id int) error {
+	stmt, err := db.Prepare("DELETE FROM posts WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	res, err := stmt.Exec(id)
+	rows, _ := res.RowsAffected()
+	if rows == 0 {
+		return errors.New("no rows affected")
+	}
+	if err != nil {
+		return err
+	}
+	return nil
 }
