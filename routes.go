29d572f5007cf3e9a50ca29dcfb40933a40433fe
mario <mariojralves2006@gmail.com>
2024-04-16 00:12:06 -0300

    Novas rotas de signup e newpost

--- routes.go
+++ routes.go
@@ -10,7 +10,9 @@ func (a *Application) Routes() http.Handler {
 	mux.HandleFunc("/login", a.LoginHandler(LoginView))
 	mux.HandleFunc("/contact", a.ContactHandler(ContactView))
 	mux.HandleFunc("/post", a.AuthMiddleware(a.PostHandler(PostView)))
+	mux.HandleFunc("/signup", a.SignUpHandler(SignUpView))
 	mux.HandleFunc("/logout", a.LogoutHandler)
+	mux.HandleFunc("/post/new", a.AuthMiddleware(a.NewPostHandler(NewPostView)))
 	mux.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("static"))))
