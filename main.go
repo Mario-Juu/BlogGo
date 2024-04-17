a7c06272beb89b56cfc0c594aac3deaf4a45bd3b
mario <mariojralves2006@gmail.com>
2024-04-16 22:26:30 -0300

    Criação automática das tabelas

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"log"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 var env = "dev"
 var cache = make(map[string]*template.Template)
 
-var LoginView *View 
-var AboutView *View 
-var ContactView *View 
-var HomeView *View 
+var LoginView *View
+var AboutView *View
+var ContactView *View
+var HomeView *View
 var PostView *View
 
 var SignUpView *View
@@ -24,77 +24,104 @@ var PostViewerView *View
 
 var db *sql.DB
 
-func CreateViews(){
+func CreateViews() {
 	var err error
-	LoginView, err = NewView( "login")
+	LoginView, err = NewView("login")
 	if err != nil {
 		log.Println(err)
 	}
-	AboutView, err = NewView( "about")
+	AboutView, err = NewView("about")
 	if err != nil {
 		log.Println(err)
 	}
-	ContactView, err = NewView( "contact")
+	ContactView, err = NewView("contact")
 	if err != nil {
 		log.Println(err)
 	}
-	HomeView, err = NewView( "index")
+	HomeView, err = NewView("index")
 	if err != nil {
 		log.Println(err)
 	}
-	PostView, err = NewView( "post")
-	if err != nil{
+	PostView, err = NewView("post")
+	if err != nil {
 		log.Println(err)
 	}
 	SignUpView, err = NewView("signup")
-	if err != nil{
+	if err != nil {
 		log.Println(err)
 	}
 
 	NewPostView, err = NewView("postnew")
-	if err != nil{
+	if err != nil {
 		log.Println(err)
 	}
 	EditPostView, err = NewView("postedit")
-	if err != nil{
+	if err != nil {
 		log.Println(err)
 	}
 	PostViewerView, err = NewView("postviewer")
-	if err != nil{
+	if err != nil {
 		log.Println(err)
 	}
 }
 
 func main() {
 	cache := make(map[string]*template.Template)
-	config := Config{Version: "1.0.0",
-	}
+	config := Config{Version: "1.0.0"}
 	flag.StringVar(&config.Port, "port", "8080", "HTTP Server Port")
 	flag.StringVar(&config.Env, "env", "dev", "Application Environment")
 	flag.Parse()
 
-	 app := Application{
-	 	Config: config,
-	 	Cache: cache,
-	 }
+	app := Application{
+		Config: config,
+		Cache:  cache,
+	}
 	CreateViews()
-	 var err error
+	var err error
 	db, err = sql.Open("mysql", "root:secret@/mysql?parseTime=true")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	db.Ping()
-	log.Println("Connected to database")
-	if err != nil{
+	err = db.Ping()
+	if err != nil {
 		log.Fatal(err)
 	}
-	
-	 
+	log.Println("Connected to database")
+	initTables()
 
 	log.Printf("Starting server from %s on :%s", config.Env, config.Port)
 
-	if err := app.Start(); err != nil{
+	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initTables() {
+	log.Println("Criando as tabelas")
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS users (
+		id int NOT NULL AUTO_INCREMENT,
+		email varchar(255) UNIQUE,
+		password varchar(255),
+		PRIMARY KEY (id)
+	);`)
+	if err != nil {
+		log.Panic(err)
+	}
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS posts(
+		id int NOT NULL AUTO_INCREMENT,
+		title varchar(255) NOT NULL,
+		slug varchar (255) NOT NULL UNIQUE,
+		content text,
+		user_id int NOT NULL,
+		created_at timestamp DEFAULT CURRENT_TIMESTAMP(),
+		updated_at timestamp DEFAULT CURRENT_TIMESTAMP(),
+		PRIMARY KEY (id),
+		FOREIGN KEY (user_id) REFERENCES users(id)
+	);
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
